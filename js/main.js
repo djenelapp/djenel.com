3f05d4c8d04daa67a7066a308346ec1d5fb1df0c
julien <jleupe@generixgroup.com>
2018-06-27 11:54:28 +0200

    27/06/18 11:54

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 $(function(){ /*DEBUT DOCUMENT READY*/
-    TweenMax.to('#blcTitleindex',2,{top: '50%',opacity: 1, delay:0.3, ease: Circ.easeOut,});/*ANIMATION 1 INDEX*/
+	TweenLite.to('.blur',2,{'-webkit-filter':'blur('+(0)+'px)', 'filter':'blur('+(0)+'px)',opacity:1, ease: Circ.easeOut,});/*ANIMATION 1 INDEX*/
+    TweenMax.to('#blcTitleindex',1.1,{top: '50%',opacity: 1, delay:0.3,  ease: Power1.easeOut,});/*ANIMATION 1 INDEX*/
 });
