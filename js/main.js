24d4e97cdfe72c093852ab51a67409e951c02874
julien <jleupe@generixgroup.com>
2018-06-27 13:46:22 +0200

    27/06/18 13:46

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function(){ /*DEBUT DOCUMENT READY*/
-	TweenLite.to('.blur',2,{'-webkit-filter':'blur('+(0)+'px)', 'filter':'blur('+(0)+'px)',opacity:1, ease: Circ.easeOut,});/*ANIMATION 1 INDEX*/
+	/*JQUERY*/
+    $(".animOpacity-In").fadeIn("300");
+	/*TWEENMAX*/
     TweenMax.to('#blcTitleindex',1.1,{top: '50%',opacity: 1, delay:0.3,  ease: Power1.easeOut,});/*ANIMATION 1 INDEX*/
 });
